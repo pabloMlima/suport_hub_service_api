d4633e56fc343d9529ee14f3c46ae45b72c7386f
Pablo <pablomlima75@gmail.com>
2024-06-30 17:40:24 -0300

    primeiro commit

---
+++ src/main/java/com/br/hubsi/suport_hub_service/SuportHubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.hubsi.suport_hub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuportHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuportHubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/hubsi/suport_hub_service/SuportHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.hubsi.suport_hub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuportHubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
